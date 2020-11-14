c4372f4a6d063ccfc1c3974de23ec94e0d739fdb
zyadelgohary <zyadgohary@hotmail.com>
2020-11-14 14:26:02 -0800

    Removed all files and assets to display an update message

--- CodingIsUs/App.js
+++ CodingIsUs/App.js
@@ -1,79 +1,44 @@
-// This is going to be the file that is launched when the app is first launched by the user. Will navigate directly to the
-// MainStackNavigator
-import 'react-native-gesture-handler';
-import React, {useRef, useEffect} from 'react';
-import MainStackNavigator from './src/MainStackNavigator';
-import {Alert} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import firebaseAnalytics from '@react-native-firebase/analytics';
-import messaging from '@react-native-firebase/messaging';
-import codePush from 'react-native-code-push';
-import {logEvent} from './config/Analytics';
-import NetInfo from '@react-native-community/netinfo';
-import strings from './config/strings';
+// This is going to display the update alert to update the app
+import React, {useEffect} from 'react';
+import {Alert, View, Platform, Linking, Text} from 'react-native';
 
-// Declares the functional component
-let App = () => {
-  // Initializes references for Firebase Analytics
-  const routeNameRef = useRef();
-  const navigationRef = useRef();
-
-  // Double checks that there is an active Internet Connection
-  // Requests Notification permissions for Firebase Cloud Messaging
+const App = () => {
   useEffect(() => {
-    logEvent('AppOpen', {});
-    useEffectHelper();
-    // Subscribe
-    const unsubscribe = NetInfo.addEventListener((networkState) => {
-      if (!networkState.isConnected) {
-        Alert.alert(strings.Whoops, strings.CodingIsUsRequiresInternet, [
-          {
-            text: strings.TryAgain,
-            onPress: () => checkInternetConnection(),
+    Alert.alert(
+      'Update Available',
+      'There is an update required for Coding is Us! Please download it now.',
+      [
+        {
+          text: 'Ok',
+          onPress: () => {
+            if (Platform.OS === 'ios') {
+              Linking.openURL(
+                'https://apps.apple.com/us/app/coding-is-us/id1532536501',
+              );
+            } else {
+              Linking.openURL(
+                'https://play.google.com/store/apps/details?id=com.CodingIsUs.CodingIsUs&hl=en&gl=US',
+              );
+            }
           },
-        ]);
-      }
-    });
-    return () => unsubscribe();
+        },
+      ],
+      {cancelable: false},
+    );
   }, []);
 
-  // Helper method for UseEffect
-  const useEffectHelper = async () => {
-    await messaging().requestPermission();
-  };
-
-  // Checks the internet connection
-  const checkInternetConnection = async () => {
-    const networkState = await NetInfo.fetch();
-    if (!networkState.isConnected) {
-      Alert.alert(strings.Whoops, strings.CodingIsUsRequiresInternet, [
-        {
-          text: strings.TryAgain,
-          onPress: () => checkInternetConnection(),
-        },
-      ]);
-    }
-  };
-
-  // Renders the UI. Wrapped by React-Navigation container
   return (
-    <NavigationContainer
-      ref={navigationRef}
-      onStateChange={async (state) => {
-        // Sets up Screen Analytics in Firebase
-        const previousRouteName = routeNameRef.current;
-        const currentRouteName = navigationRef.current.getCurrentRoute().name;
-        if (previousRouteName !== currentRouteName) {
-          await firebaseAnalytics().logScreenView({
-            screen_name: currentRouteName,
-            screen_class: currentRouteName,
-          });
-        }
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
       }}>
-      <MainStackNavigator />
-    </NavigationContainer>
+      <Text style={{fontSize: 20, fontWeight: 'bold'}}>
+        Please install required update
+      </Text>
+    </View>
   );
 };
 
-App = codePush(App);
 export default App;
--- CodingIsUs/config/dimensions.js
+++
@@ -1,7 +0,0 @@
-// This is going to export the height and the width of the screen to the rest of the app's files as named exports
-import {Dimensions} from 'react-native';
-
-const screenHeight = Dimensions.get('window').height;
-const screenWidth = Dimensions.get('window').width;
-
-export {screenHeight, screenWidth};
